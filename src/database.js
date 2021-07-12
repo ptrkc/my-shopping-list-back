6da84948a5240526f4b1a9cccfc62cd21ce08f8a
ptrkc <40273827+ptrkc@users.noreply.github.com>
2021-07-12 14:32:35 -0300

    Added .env, tests and package.json configs

--- src/database.js
+++ src/database.js
@@ -1,13 +1,18 @@
 import pg from "pg";
+import dotenv from "dotenv";
+dotenv.config();
 
 const { Pool } = pg;
 
 const connection = new Pool({
-  user: "bootcamp_role",
-  password: "senha_super_hiper_ultra_secreta_do_role_do_bootcamp",
-  host: "localhost",
-  port: 5432,
-  database: "my_shopping_list"
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database:
+        process.env.NODE_ENV === "test"
+            ? process.env.DB_TEST_DATABASE
+            : process.env.DB_DATABASE,
 });
 
 export default connection;
